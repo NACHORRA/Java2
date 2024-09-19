68e03ecedfaca94e85d800519fe0198489202a59
NACHORRA <ignaciogomez047@gmail.com>
2024-09-19 09:55:05 -0300

    menu

--- entrega2/js/pag.js
+++ entrega2/js/pag.js
@@ -1,9 +1,17 @@
 //uso las func de selectors para agarrar las cosas con esas id. ahora digamos las modifico con el java. Con body hago lo mismo pero conn document
-
-const btnMenu = select('#btn-menu')
-const mobileMenu = select('nav')
 const body = document.body
 const btnCart = select('#btn-cart')
 const cartElement = select("#cart")
+const _items = select('#_items')
+const _toggle = select('#_toggle')
+
+
+//activo, desactivo menu
+
+_toggle.onclick = () =>{
+    _items.classList.toggle("open")
+    _toggle.classList.toggle("close")
+}
+
 
 
